36e84e8c2291410471546a23afea2c1ae4e206ff
Leriv <leonardo.rivadeneira@hotmail.com>
2024-04-20 21:41:52 -0500

    Se elimino la barra lateral derecha en Articles Cambios en la pestaña de eventos

--- src/components/articles/Articles.vue
+++ src/components/articles/Articles.vue
@@ -5,11 +5,11 @@
 
         <div class="row g-5">
 
-          <div class="col-lg-8" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-12" data-aos="fade-up" data-aos-delay="200">
 
             <div class="row gy-5 posts-list">
 
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <article class="d-flex flex-column">
 
                   <div class="post-img">
@@ -41,7 +41,7 @@
                 </article>
               </div><!-- End post list item -->
 
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <article class="d-flex flex-column">
 
                   <div class="post-img">
@@ -73,7 +73,7 @@
                 </article>
               </div><!-- End post list item -->
 
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <article class="d-flex flex-column">
 
                   <div class="post-img">
@@ -105,7 +105,7 @@
                 </article>
               </div><!-- End post list item -->
 
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <article class="d-flex flex-column">
 
                   <div class="post-img">
@@ -137,7 +137,7 @@
                 </article>
               </div><!-- End post list item -->
 
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <article class="d-flex flex-column">
 
                   <div class="post-img">
@@ -169,7 +169,7 @@
                 </article>
               </div><!-- End post list item -->
 
-              <div class="col-lg-6">
+              <div class="col-lg-4">
                 <article class="d-flex flex-column">
 
                   <div class="post-img">
@@ -210,97 +210,6 @@
                 <li><a href="#">3</a></li>
               </ul>
             </div><!-- End blog pagination -->
-
-          </div>
-
-          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="400">
-
-            <div class="sidebar ps-lg-4">
-
-              <div class="sidebar-item search-form">
-                <h3 class="sidebar-title">Buscar</h3>
-                <form action="" class="mt-3">
-                  <input type="text">
-                  <button type="submit"><i class="bi bi-search"></i></button>
-                </form>
-              </div><!-- End sidebar search formn-->
-
-              <div class="sidebar-item categories">
-                <h3 class="sidebar-title">Categorías</h3>
-                <ul class="mt-3">
-                  <li><a href="#">Ciencia de datos <span>(12)</span></a></li>
-                  <li><a href="#">Inteligencia artificial <span>(5)</span></a></li>
-                  <li><a href="#">Ciberseguridad <span>(22)</span></a></li>
-                </ul>
-              </div><!-- End sidebar categories-->
-
-              <div class="sidebar-item recent-posts">
-                <h3 class="sidebar-title">Recent Posts</h3>
-
-                <div class="mt-3">
-
-                  <div class="post-item mt-3">
-                    <img src="../../assets/img/blog/blog-recent-1.jpg" alt="" class="flex-shrink-0">
-                    <div>
-                      <h4><a href="blog-post.html">Nihil blanditiis at in nihil autem</a></h4>
-                      <time datetime="2020-01-01">Jan 1, 2020</time>
-                    </div>
-                  </div><!-- End recent post item-->
-
-                  <div class="post-item">
-                    <img src="../../assets/img/blog/blog-recent-2.jpg" alt="" class="flex-shrink-0">
-                    <div>
-                      <h4><a href="blog-post.html">Quidem autem et impedit</a></h4>
-                      <time datetime="2020-01-01">Jan 1, 2020</time>
-                    </div>
-                  </div><!-- End recent post item-->
-
-                  <div class="post-item">
-                    <img src="../../assets/img/blog/blog-recent-3.jpg" alt="" class="flex-shrink-0">
-                    <div>
-                      <h4><a href="blog-post.html">Id quia et et ut maxime similique occaecati ut</a></h4>
-                      <time datetime="2020-01-01">Jan 1, 2020</time>
-                    </div>
-                  </div><!-- End recent post item-->
-
-                  <div class="post-item">
-                    <img src="../../assets/img/blog/blog-recent-4.jpg" alt="" class="flex-shrink-0">
-                    <div>
-                      <h4><a href="blog-post.html">Laborum corporis quo dara net para</a></h4>
-                      <time datetime="2020-01-01">Jan 1, 2020</time>
-                    </div>
-                  </div><!-- End recent post item-->
-
-                  <div class="post-item">
-                    <img src="../../assets/img/blog/blog-recent-5.jpg" alt="" class="flex-shrink-0">
-                    <div>
-                      <h4><a href="blog-post.html">Et dolores corrupti quae illo quod dolor</a></h4>
-                      <time datetime="2020-01-01">Jan 1, 2020</time>
-                    </div>
-                  </div><!-- End recent post item-->
-
-                </div>
-
-              </div><!-- End sidebar recent posts-->
-
-              <div class="sidebar-item tags">
-                <h3 class="sidebar-title">Tags</h3>
-                <ul class="mt-3">
-                  <li><a href="#">App</a></li>
-                  <li><a href="#">IT</a></li>
-                  <li><a href="#">Business</a></li>
-                  <li><a href="#">Mac</a></li>
-                  <li><a href="#">Design</a></li>
-                  <li><a href="#">Office</a></li>
-                  <li><a href="#">Creative</a></li>
-                  <li><a href="#">Studio</a></li>
-                  <li><a href="#">Smart</a></li>
-                  <li><a href="#">Tips</a></li>
-                  <li><a href="#">Marketing</a></li>
-                </ul>
-              </div><!-- End sidebar tags-->
-
-            </div><!-- End Blog Sidebar -->
 
           </div>
 
--- src/components/events/Event.vue
+++ src/components/events/Event.vue
@@ -1,19 +1,28 @@
 <template>
-    <!-- ======= Acticles Section ======= -->
-    <section id="blog" class="blog">
-      <div class="container" data-aos="fade-up">
-        <div class="row d-flex justify-content-center">
-          <div class="col-lg-5">
-            <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FGICDIAC%2Fposts%2Fpfbid02Eq44DLD9gDUE5GPqCXHE2w1q4m7EAbNYFAEZ2aSfHk5XRMTmgrUEx9K2YtRhDB6gl&show_text=true&width=500" width="900" height="697" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-          </div>
+  <!-- ======= Acticles Section ======= -->
+  <section id="blog" class="blog">
+    <div class="container" data-aos="fade-up">
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-5">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FGICDIAC&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+            width="500"
+            height="500"
+            style="border: none; overflow: hidden"
+            scrolling="no"
+            frameborder="0"
+            allowfullscreen="true"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          ></iframe>
         </div>
       </div>
-    </section><!-- End Acticles Section -->
+    </div>
+  </section>
+  <!-- End Acticles Section -->
 </template>
-<script setup>
-</script>
+<script setup></script>
 <style scoped>
- a {
-  text-decoration: none;  
- }
+a {
+  text-decoration: none;
+}
 </style>
