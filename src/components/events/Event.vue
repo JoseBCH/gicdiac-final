8a10eac682d77be9764d79e37ce5ec5458c0ef96
Leriv <leonardo.rivadeneira@hotmail.com>
2024-04-20 21:56:03 -0500

    Cambios en events, Biografia y eventos

--- src/components/events/Event.vue
+++ src/components/events/Event.vue
@@ -15,6 +15,18 @@
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
           ></iframe>
         </div>
+        <div class="col-lg-5">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FGICDIAC&tabs=events&width=500&height=131&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=false&appId"
+            width="500"
+            height="131"
+            style="border: none; overflow: hidden"
+            scrolling="no"
+            frameborder="0"
+            allowfullscreen="true"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          ></iframe>
+        </div>
       </div>
     </div>
   </section>
