c78ff9794d560e89972c3fa6c7eaae757ccdc6c8
Leriv <leonardo.rivadeneira@hotmail.com>
2024-06-22 23:42:57 -0500

    Cambios en el mapa

--- src/components/contacts/Contacts.vue
+++ src/components/contacts/Contacts.vue
@@ -40,7 +40,7 @@
 
                 <div class="col-lg-6 ">
                     <iframe class="mb-4 mb-lg-0"
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15850.030111544575!2d-79.9042231!3d-6.7077407!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x904ced9307db00dd%3A0xd82bb63ce69b1c52!2sUniversidad%20Nacional%20Pedro%20Ruiz%20Gallo!5e0!3m2!1ses!2spe!4v1713392438965!5m2!1ses!2spe"
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d906.7841212956911!2d-79.9080814566705!3d-6.708085474494651!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwNDInMjkuMiJTIDc5wrA1NCcyOS40Ilc!5e1!3m2!1ses!2spe!4v1719117338627!5m2!1ses!2spe"
                         frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
                 </div>
 
